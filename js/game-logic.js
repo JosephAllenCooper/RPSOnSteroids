15644b86c6c1ad8d29abaeb198adf1382a89185b
Joseph Cooper <joe@jcooper.id.au>
2020-08-29 23:50:31 +1000

    Global variables, player moves set and round winner determined.

--- js/game-logic.js
+++ js/game-logic.js
@@ -1,1 +1,126 @@
-// All code should be written in this file.
+let playerOneMoveOneType = undefined;
+let playerOneMoveTwoType = undefined;
+let playerOneMoveThreeType = undefined;
+
+let playerOneMoveOneValue = undefined;
+let playerOneMoveTwoValue = undefined;
+let playerOneMoveThreeValue = undefined;
+
+let playerTwoMoveOneType = undefined;
+let playerTwoMoveTwoType = undefined;
+let playerTwoMoveThreeType = undefined;
+
+let playerTwoMoveOneValue = undefined;
+let playerTwoMoveTwoValue = undefined;
+let playerTwoMoveThreeValue = undefined;
+
+function setPlayerMoves(player, moveOneType, moveOneValue, moveTwoType, moveTwoValue, moveThreeType, moveThreeValue) {
+  if (player === 'Player One') {
+    playerOneMoveOneType = moveOneType;
+    playerOneMoveOneValue = moveOneValue;
+    playerOneMoveTwoType = moveTwoType;
+    playerOneMoveTwoValue = moveTwoValue;
+    playerOneMoveThreeType = moveThreeType;
+    playerOneMoveThreeValue = moveThreeValue;
+  } else if (player === 'Player Two') {
+    PlayerTwoMoveOneType = moveOneType;
+    PlayerTwoMoveOneValue = moveOneValue;
+    PlayerTwoMoveTwoType = moveTwoType;
+    PlayerTwoMoveTwoValue = moveTwoValue;
+    PlayerTwoMoveThreeType = moveThreeType;
+    PlayerTwoMoveThreeValue = moveThreeValue;
+  }
+}
+
+function getRoundWinner (roundNumber) {
+  playerOneMove = '';
+  playerTwoMove = '';
+  playerOneValue = '';
+  playerTwoValue= '';
+
+  switch (roundNumber) {
+    case 1:
+      playerOneMove = playerOneMoveOneType;
+      playerOneValue = playerOneMoveOneValue;
+
+      playerTwoMove =  playerTwoMoveOneType;
+      playerTwoValue = playerTwoMoveOneValue;
+      break;
+    
+    case 2:
+      playerOneMove = playerOneMoveTwoType;
+      playerOneValue = playerOneMoveTwoValue;
+
+      playerTwoMove =  playerTwoMoveTwoType;
+      playerTwoValue = playerTwoMoveTwoValue;
+      break;
+    
+    case 3:
+      playerOneMove = playerOneMoveThreeType;
+      playerOneValue = playerOneMoveThreeValue;
+
+      playerTwoMove =  playerTwoMoveThreeType;
+      playerTwoValue = playerTwoMoveThreeValue;
+      break;
+  
+    default:
+      return 'Tie';
+  }
+
+  switch (playerOneMove) {
+    case 'rock':
+      if (playerTwoMove === 'paper') {
+        return 'Player Two';
+      } else if (playerTwoMove === 'scissors') {
+        return 'Player One';
+      } else if (platerTwoMove === 'rock') {
+        if (playerOneValue === playerTwoValue) {
+          return 'Tie';
+        } else if (playerOneValue > playerTwoValue) {
+          return 'Player One';
+        } else if (playerOneValue < playerTwoValue) {
+          return 'Player Two'
+        }
+      }
+      break;
+
+    case 'paper':
+      if (playerTwoMove === 'scissors') {
+        return 'Player Two';
+      } else if (playerTwoMove === 'rock') {
+        return 'Player One';
+      } else if (platerTwoMove === 'paper') {
+        if (playerOneValue === playerTwoValue) {
+          return 'Tie';
+        } else if (playerOneValue > playerTwoValue) {
+          return 'Player One';
+        } else if (playerOneValue < playerTwoValue) {
+          return 'Player Two'
+        }
+      }
+      break;
+
+    case 'scissors':
+      if (playerTwoMove === 'rock') {
+        return 'Player Two';
+      } else if (playerTwoMove === 'paper') {
+        return 'Player One';
+      } else if (platerTwoMove === 'scissors') {
+        if (playerOneValue === playerTwoValue) {
+          return 'Tie';
+        } else if (playerOneValue > playerTwoValue) {
+          return 'Player One';
+        } else if (playerOneValue < playerTwoValue) {
+          return 'Player Two'
+        }
+      }
+      break;
+  
+    default:
+      return 'error in determining round winner.';
+  }
+}
+
+function getGameWinner(params) {
+  
+}

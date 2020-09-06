3f585ca2574b47e39711518cfdeedbd80a0c55bb
Joseph Cooper <joe@jcooper.id.au>
2020-09-06 23:00:52 +1000

    Added random computer logic - project complete

--- js/game-logic.js
+++ js/game-logic.js
@@ -163,3 +163,14 @@ function getGameWinner() {
     return 'Player Two';
   }
 }
+
+function setComputerMoves() {
+  const moves = ['rock', 'paper', 'scissors'];
+  const moveOneType = moves[Math.floor(Math.random() * 3)];
+  const moveTwoType = moves[Math.floor(Math.random() * 3)];
+  const moveThreeType = moves[Math.floor(Math.random() * 3)];
+  const moveOneValue = Math.floor(Math.random() * 96) + 1;
+  const moveTwoValue = Math.floor(Math.random() * (97 - moveOneValue)) + 1;
+  const moveThreeValue = 99 - moveOneValue - moveTwoValue;
+  setPlayerMoves('Player Two', moveOneType, moveOneValue, moveTwoType,moveTwoValue, moveThreeType, moveThreeValue);
+}
